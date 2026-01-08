648c97da67372c4778c23253f2460b37f900b450
AlbaGV2 <albagarcia@iesbalmasedabhi.com>
2026-01-08 13:55:05 +0100

    Agregar archivos de proyecto.

---
+++ BibliotecaMVC/Core/Entities/Libro.cs
@@ -0,0 +1,12 @@
+﻿namespace BibliotecaMVC.Core.Entities
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AnioPunlicacion { get; set; }
+
+
+    }
+}
---
+++ BibliotecaMVC/Core/Entities/Usuario.cs
@@ -0,0 +1,17 @@
+﻿namespace BibliotecaMVC.Core.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Email { get; set; }
+
+        public List<Rol> Roles { get; set; }
+
+        public string PasswordHash
+        {
+            get; set;
+
+        }
+    }
+}
