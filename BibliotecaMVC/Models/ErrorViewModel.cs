d5d919c7867858549e47b7035a9a5b0b1b866aaa
Alba Garcia <albagarcia@iesbalmasedabhi.com>
2026-01-29 13:06:57 +0100

    Delete BibliotecaMVC directory

--- BibliotecaMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BibliotecaMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
